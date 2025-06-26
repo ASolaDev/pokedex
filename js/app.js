b905e360f259801c6e800e8b73872f4cab966e77
ASolaDev <antoniosola991@gmail.com>
2025-06-26 09:02:08 +0200

    cambiado a 251 el numero de pokemon

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const botonesHeader = document.querySelectorAll(".btn-header");
 
 let URL = "https://pokeapi.co/api/v2/pokemon/";
 
-for (let i = 1; i <= 151; i++) {
+for (let i = 1; i <= 251; i++) {
     fetch(URL + i)
         .then((response) => response.json())
         .then(data => mostrarPokemon(data))
